5aac49a49dd0f203fbfd5b071f15d750ae17b9a5
Iltwats <2012atulsharma@gmail.com>
2023-07-16 23:52:07 +0530

    Model and example data

---
+++ src/main/kotlin/com/backend/api/springbootkotlin/model/CourseList.kt
@@ -0,0 +1,13 @@
+package com.backend.api.springbootkotlin.model
+
+object CourseList {
+    val courseList = mutableListOf(
+        Courses(12, "name", "desc"),
+        Courses(13, "neme6", "derc"),
+        Courses(14, "neme7", "derc"),
+        Courses(15, "neme6", "derc"),
+        Courses(16, "neme63", "derc"),
+        Courses(17, "neme4", "derc"),
+        Courses(18, "neme2", "derc")
+    )
+}
---
+++ src/main/kotlin/com/backend/api/springbootkotlin/model/Courses.kt
@@ -0,0 +1,7 @@
+package com.backend.api.springbootkotlin.model
+
+data class Courses(
+    val id: Int,
+    val title: String,
+    val description: String
+)
